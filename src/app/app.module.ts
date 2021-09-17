670cdcfeb7d9f6087b6ef4f7e09fcadb6b966cdf
covuworie <chuk.ovuworie@gmail.com>
2021-09-16 21:08:33 -0500

    Install ngrx store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +17,10 @@ import { reducers, metaReducers } from './store';
     SharedModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
     }),
   ],
   providers: [],
