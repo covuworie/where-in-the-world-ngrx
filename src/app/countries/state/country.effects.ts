6516d72ff972f24bfcf06954d7b54006e2560535
covuworie <chuk.ovuworie@gmail.com>
2021-09-28 19:16:02 -0500

    Add countries data to local storage on successfull API call

--- src/app/countries/state/country.effects.ts
+++ src/app/countries/state/country.effects.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { RestCountriesService } from '../shared/rest-countries.service';
 import * as CountryActions from './country.actions';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
+import { LocalStorageService } from 'src/app/shared/local-storage.service';
 
 @Injectable()
 export class CountryEffects {
@@ -15,6 +16,14 @@ export class CountryEffects {
         this.restCountriesService.getAll().pipe(
           map((countries) =>
             CountryActions.loadCountriesSuccess({ countries })
+          ),
+          tap((countries) =>
+            LocalStorageService.setItemWithExpiry(
+              'countries',
+              JSON.stringify(countries),
+              // 30 days time to live (TTL) as the countries data is updated on the order of months
+              1000 * 60 * 60 * 24 * 30
+            )
           ),
           catchError((error: HttpErrorResponse) =>
             of(CountryActions.loadCountriesFailure({ error }))
