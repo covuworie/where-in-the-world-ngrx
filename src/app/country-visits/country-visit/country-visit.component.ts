fd384edd3fd2a7008b6728870ef5416dc3f8eea9
covuworie <chuk.ovuworie@gmail.com>
2021-10-14 14:55:59 -0500

    Load hardcoded country visits view

--- src/app/country-visits/country-visit/country-visit.component.ts
+++ src/app/country-visits/country-visit/country-visit.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { forbiddenCountryValidator } from 'src/app/countries/shared/forbidden-country.directive';
+import { forbiddenMaxDurationValidator } from 'src/app/countries/shared/max-duration.directive';
+import { YearsService } from 'src/app/countries/shared/years.service';
 import CountryVisitViewModel from './country-visit-view.model';
 
 @Component({
@@ -7,15 +10,57 @@ import CountryVisitViewModel from './country-visit-view.model';
   templateUrl: './country-visit.component.html',
   styleUrls: ['./country-visit.component.scss'],
 })
-export class CountryVisitComponent {
+export class CountryVisitComponent implements OnInit {
   // public properties
   @Input() countryVisit: CountryVisitViewModel = {
     year: 0,
     country: '',
     duration: 0,
   };
+  @Input() validCountryNames: string[] = [];
   visit = this.fb.group(this.countryVisit);
 
   // public methods
   constructor(private fb: FormBuilder) {}
+
+  get currentYear() {
+    return YearsService.currentYear;
+  }
+
+  getMaxDuration(year: number) {
+    return YearsService.maxDays(year);
+  }
+
+  get minYear() {
+    return YearsService.minYear;
+  }
+
+  ngOnInit() {
+    this.setFormControls(this.countryVisit);
+  }
+
+  // private methods
+  private setFormControls(visit: CountryVisitViewModel) {
+    this.visit = this.fb.group(
+      {
+        year: [
+          visit.year,
+          [
+            Validators.required,
+            Validators.min(this.currentYear - 125),
+            Validators.max(this.currentYear),
+          ],
+        ],
+        country: [
+          visit.country,
+          [
+            Validators.required,
+            forbiddenCountryValidator(this.validCountryNames),
+          ],
+        ],
+        duration: [visit.duration, [Validators.required, Validators.min(1)]],
+      },
+      { validators: [forbiddenMaxDurationValidator] }
+    );
+  }
 }
--- src/app/country-visits/country-visits.component.ts
+++ src/app/country-visits/country-visits.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Store } from '@ngrx/store';
 import CountryVisitViewModel from './country-visit/country-visit-view.model';
+import * as CountrySelectors from '../countries/state/country.selectors';
+import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-countries-visited',
   templateUrl: './country-visits.component.html',
   styleUrls: ['./country-visits.component.scss'],
 })
-export class CountryVisitsComponent {
+export class CountryVisitsComponent implements OnInit {
   // public properties
-  countryVisits: CountryVisitViewModel[] = [];
+  vm$: Observable<CountryVisitViewModel[]> = of([
+    { year: 2000, country: 'France', duration: 15 },
+    { year: 2010, country: 'Germany', duration: 10 },
+  ]);
   faPlus = faPlus;
+  validCountryNames: string[] = [];
 
   // public methods
-  constructor() {}
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    // make sure countries list is loaded
+    this.store
+      .select(CountrySelectors.selectCommonNames)
+      .pipe(
+        tap((validCountryNames) => (this.validCountryNames = validCountryNames))
+      );
+  }
+
+  onAddVisit() {}
 }
