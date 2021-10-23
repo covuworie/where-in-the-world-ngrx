856438aaca871b35646ff0b4dba9c5ee887df747
covuworie <chuk.ovuworie@gmail.com>
2021-10-23 16:40:37 -0500

    Add spinner effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,13 +16,17 @@ import { CountriesEffects } from './store/effects/country.effects';
 import { RouterEffects } from './store/effects/router.effects';
 import { InitEffects } from './store/effects/init.effects';
 import { UserSettingsEffects } from './store/effects/user-settings.effects';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SpinnerEffects } from './store/effects/spinner.effects';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    NgxSpinnerModule,
     SharedModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
@@ -34,6 +39,7 @@ import { UserSettingsEffects } from './store/effects/user-settings.effects';
       CountriesEffects,
       InitEffects,
       RouterEffects,
+      SpinnerEffects,
       UserSettingsEffects,
       WishListEffects,
     ]),
