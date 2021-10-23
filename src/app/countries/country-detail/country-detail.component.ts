87f51c9a075e82fbd942e35437e5fc1a2c70a9ff
covuworie <chuk.ovuworie@gmail.com>
2021-10-23 14:08:33 -0500

    Redirect to countries page if country is not found

--- src/app/countries/country-detail/country-detail.component.ts
+++ src/app/countries/country-detail/country-detail.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import CountryDetailViewModel from './country-detail-view.model';
+import * as CountryDetailActions from '../state/country-detail.actions';
 import * as CountrySelectors from '../../store/selectors/country.selectors';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-country-detail',
@@ -21,9 +23,22 @@ export class CountryDetailComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       const path = params.get('name')!;
       const commonName = path.replace(/-/g, ' ');
-      this.vm$ = this.store.select(
-        CountrySelectors.selectCountryDetailViewModel(commonName)
-      );
+      this.store
+        .select(CountrySelectors.selectCommonNameExists(commonName))
+        .pipe(
+          tap((countryExists) => {
+            if (!countryExists) {
+              this.store.dispatch(
+                CountryDetailActions.countryNotFoundInStore()
+              );
+            } else {
+              this.vm$ = this.store.select(
+                CountrySelectors.selectCountryDetailViewModel(commonName)
+              );
+            }
+          })
+        )
+        .subscribe();
     });
   }
 }
